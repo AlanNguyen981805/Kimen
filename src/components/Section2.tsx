70eeef2a39a7a4442c0e108711ed385179e90c21
hiep <hiepdev.js@gmail.com>
2024-02-03 17:13:32 +0700

    check

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -20,7 +20,7 @@ const Section2 = ({ cate, desc, title, imgUrl, isShowList = true }: IProps) => {
   };
   return (
     <div className="w-full flex px-[80px] mt-4">
-      <div className="w-3/5 relative mx-auto flex justify-center items-center">
+      <div className="w-1/2 relative mx-auto flex justify-center items-center">
         <div className="max-w-[400px]">
           <div>
             <span className="text-xl text-[#4b4b4b] italic">{cate}</span>
@@ -39,7 +39,7 @@ const Section2 = ({ cate, desc, title, imgUrl, isShowList = true }: IProps) => {
           )}
         </div>
       </div>
-      <div className="w-2/5 flex items-start gap-4 justify-center flex-col">
+      <div className="w-1/2 flex items-start gap-4 justify-center flex-col">
         <Image
           src={imgUrl}
           width={300}
